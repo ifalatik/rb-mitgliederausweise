0f2330588d29a0b8fa351ac6cd2ec259a3b24d2c
Ian Paul Falatik <63957260+ifalatik@users.noreply.github.com>
2025-07-28 20:45:48 +0200

    make start and end number configurable

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import math
 
+import click
 from PIL.Image import Image
 
 from utils.google import get_worksheet_data
@@ -56,7 +57,10 @@ def generate_all_sheets(cards: list[Image], output_dir="output", columns=2, rows
         sheet.save(output_path)
 
 
-def main():
+@click.command()
+@click.option('-s', '--start-mitgliedsnummer', default=None, help='Start Mitgliedsnummer for filtering')
+@click.option('-e', '--end-mitgliedsnummer', default=None, help='End Mitgliedsnummer for filtering')
+def main(start_mitgliedsnummer: str, end_mitgliedsnummer: str):
     data = get_worksheet_data(SERVICE_ACCOUNT_FILE)
     # skip first row
     data = data[1:]
@@ -68,6 +72,26 @@ def main():
         # check if the category is valid
         if row.get("Mitglieder-kategorie") not in VALID_CATEGORIES:
             print(f"Skipping row with invalid category: {row.get('Mitglieder-kategorie')}")
+            continue
+
+        mitgliedsnummer = row.get("Mitgliedsnummer")
+        if not mitgliedsnummer:
+            print("Skipping row with missing Mitgliedsnummer")
+            continue
+        try:
+            mitgliedsnummer = int(mitgliedsnummer)
+        except ValueError:
+            print(f"Skipping row with invalid Mitgliedsnummer: {row.get('Mitgliedsnummer')}")
+            continue
+
+        # check if the Mitgliedsnummer is greater or equal to the start Mitgliedsnummer
+        if start_mitgliedsnummer and mitgliedsnummer < int(start_mitgliedsnummer):
+            print(f"Skipping row with Mitgliedsnummer {row.get('Mitgliedsnummer')} less than {start_mitgliedsnummer}")
+            continue
+
+        # check if the Mitgliedsnummer is less than or equal to the end Mitgliedsnummer
+        if end_mitgliedsnummer and mitgliedsnummer > int(end_mitgliedsnummer):
+            print(f"Skipping row with Mitgliedsnummer {row.get('Mitgliedsnummer')} greater than {end_mitgliedsnummer}")
             continue
 
         card_image = generate_card_image(row)
